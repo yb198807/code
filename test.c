97b29450f05d5aba7b53cadf720e3036ead63aa1
yb198807 <yebin4@sina.com>
2023-07-13 17:53:20 +0800

    hello

--- test.c
+++ test.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-typedef struct stu
-{
-    char name[20];
-    int age;
-    char sex[5];
-}Stu;
+// int *twoSum(int *nums, int numsSize, int target)
+// {
+//     int i, j;
+//     for (i = 0; i < j; i++)
+//     {
+//         for (j = i + 1; j < numsSize; j++)
+//         {
+//             if (nums[i] + nums[j] == target)
+//             {
+//                 int arr[2] = {i, j};
+//                 return arr;
+//             }
+//         }
+//     }
+// }
 int main()
 {
-    Stu stu={"chen",18,"nv"};
-    Stu* pstu=&stu;
-    scanf("%d",&pstu->age);
-    printf("%d\n",stu.age);
-    printf("%d\n",pstu->age);
+    int arr[]={1,2,3,4,5};
+    int* p=arr;
+    int a;
+    for(a=0;a<5;a++)
+    {
+        printf("%d\n",*(p+a));
+    }
     return 0;
 }
