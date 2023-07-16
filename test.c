8ccbd39ab7f22edca531c671ba67092a31d0c943
yb198807 <yebin4@sina.com>
2023-07-16 17:57:33 +0800

    hello

--- test.c
+++ test.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+int fun(int a, int b)
+{
+	int sum = 1;
+	while (b--)
+	{
+		sum *= a;
+	}
+	return sum;
+}
 int main()
 {
-	int a;
-	scanf("%d", &a);
+	int a = 2, b = 4;
+	int sum = fun(a, b);
+	printf("%d\n", sum);
 	return 0;
 }
