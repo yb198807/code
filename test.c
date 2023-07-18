3661a0190236237b86adc9a514191e5bce6bac88
yb198807 <yebin4@sina.com>
2023-07-18 10:17:24 +0800

    hello

--- test.c
+++ test.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
-int fun(int a, int b)
+#include <string.h>
+int find(int(*arr)[4],int n)
 {
-	int sum = 1;
-	while (b--)
+	int i;
+	for(i=0;i<3;i++)
 	{
-		sum *= a;
+		if(arr[i][3]>n)
+		break;
 	}
-	return sum;
+	int j;
+	for(j=3;j>=0;j--)
+	{
+		if(arr[i][j]==n)
+		return 1;
+	}
+	return 0;
 }
 int main()
 {
-	int a = 2, b = 4;
-	int sum = fun(a, b);
-	printf("%d\n", sum);
+	int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
+	int n=7;
+	int a=find(arr,n);
+	printf("%s\n", a==1?"Yes":"No");
 	return 0;
 }
