be13d7fdddce205bc7f69ef049dc1cc062860acd
yb198807 <yebin4@sina.com>
2023-07-19 10:00:59 +0800

    hello

--- test.c
+++ test.c
@@ -1,71 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-void Nextval(char T[], int *next)
+void cpy(void* b,const void* a,int n)
 {
-	int lenT = strlen(T);
-	int k = -1; // 前缀
-	int j = 0;	// 后缀
-	next[0] = -1;
-	while (j < lenT)
+	int i;
+	for(i=0;i<n;i++)
 	{
-		if (k == -1 || T[j] == T[k])
-		{
-			j++;
-			k++;
-			// 较之前next数组求法，改动在下面4行
-			if (T[j] != T[k])
-			{
-				next[j] = k; // 之前只有这一行
-			}
-			else
-			{
-				next[j] = next[k]; /*为不能出现T[j] = T[ next[j ]]，
-									 所以当出现时连续进行两次回溯，k = next[next[k]]  */
-			}
-		}
-		else
-		{
-			k = next[k];
-		}
-	}
-}
-int KMP(char S[], char T[])
-{
-	int i = 0, j = 0, lenS, lenT;
-	lenS = strlen(S);
-	lenT = strlen(T);
-	int next[30];
-	Nextval(T, next);
-	while (i < lenS && j < lenT)
-	{
-		if (j == -1 || S[i] == T[j])
-		{
-			i++;
-			j++;
-		}
-		else
-		{
-			j = next[j];
-		}
-	}
-	if (j == lenT)
-	{
-		printf("Match succeed!\n");
-		return i - j;
-	}
-	else
-	{
-		printf("Match failed!\n");
-		return -1;
+		*(char*)b++=*(char*)a++;
 	}
 }
 int main()
 {
-	char S[30], T[30];
-	int target;
-	strcpy(S, "AAAAAAAAAAttract");
-	strcpy(T, "Attract");
-	target = KMP(S, T);
-	printf("Position is %d\n", target);
+	int a[]={1,2,3,4,5};
+	int b[5]={0};
+	cpy(b,a,sizeof(a));
 	return 0;
 }
