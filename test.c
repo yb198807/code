c8dbdea3e91ca5809f45ee3f27457e91a2548bce
yb198807 <yebin4@sina.com>
2023-07-19 09:21:45 +0800

    hello

--- test.c
+++ test.c
@@ -1,26 +1,71 @@
 #include <stdio.h>
 #include <string.h>
-int find(int(*arr)[4],int n)
+void Nextval(char T[], int *next)
 {
-	int i;
-	for(i=0;i<3;i++)
+	int lenT = strlen(T);
+	int k = -1; // 前缀
+	int j = 0;	// 后缀
+	next[0] = -1;
+	while (j < lenT)
 	{
-		if(arr[i][3]>n)
-		break;
+		if (k == -1 || T[j] == T[k])
+		{
+			j++;
+			k++;
+			// 较之前next数组求法，改动在下面4行
+			if (T[j] != T[k])
+			{
+				next[j] = k; // 之前只有这一行
+			}
+			else
+			{
+				next[j] = next[k]; /*为不能出现T[j] = T[ next[j ]]，
+									 所以当出现时连续进行两次回溯，k = next[next[k]]  */
+			}
+		}
+		else
+		{
+			k = next[k];
+		}
 	}
-	int j;
-	for(j=3;j>=0;j--)
+}
+int KMP(char S[], char T[])
+{
+	int i = 0, j = 0, lenS, lenT;
+	lenS = strlen(S);
+	lenT = strlen(T);
+	int next[30];
+	Nextval(T, next);
+	while (i < lenS && j < lenT)
 	{
-		if(arr[i][j]==n)
-		return 1;
+		if (j == -1 || S[i] == T[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			j = next[j];
+		}
 	}
-	return 0;
+	if (j == lenT)
+	{
+		printf("Match succeed!\n");
+		return i - j;
+	}
+	else
+	{
+		printf("Match failed!\n");
+		return -1;
+	}
 }
 int main()
 {
-	int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
-	int n=7;
-	int a=find(arr,n);
-	printf("%s\n", a==1?"Yes":"No");
+	char S[30], T[30];
+	int target;
+	strcpy(S, "AAAAAAAAAAttract");
+	strcpy(T, "Attract");
+	target = KMP(S, T);
+	printf("Position is %d\n", target);
 	return 0;
 }
